450007fcfcb325d46c1b8410078c6d35208c14c0
Knyak <ufshg@naver.com>
2023-10-03 22:15:24 +0900

    add background (dummy)

--- Scripts/GameScripts/Game.gd
+++ Scripts/GameScripts/Game.gd
@@ -2,13 +2,16 @@ extends Node2D
 
 var fire_count = 0
 var fire_count_box
+var temp
 
 
 func _ready():
 	fire_count_box = get_node("Panel/Fire_count")
+	temp = get_node("Node2D/Sprite2D")
 
 
 func _process(delta):
+	temp.position = get_global_mouse_position()
 	pass
 
 
@@ -21,5 +24,6 @@ func _on_player_shoot(Bullet, direction, location):
 	add_child(bullet)
 	bullet.rotation = get_angle_to(direction)
 	bullet.position = location
-	bullet.velocity = direction * 500
+	# direction x bullet speed
+	bullet.velocity = direction * 200
 	_add_score()
