032d94b2e1df85c95c749e79e636d2d9c2ece4ac
Haesol Choi <81071456+NyaNyak@users.noreply.github.com>
2023-11-29 23:23:36 +0900

    배경음/효과음 추가

--- Scripts/Start.gd
+++ Scripts/Start.gd
@@ -7,6 +7,8 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Bgm.stream = load("res://assets/sound/gunfight.mp3")
+	Bgm.play()
 	pass # Replace with function body.
 
 
@@ -16,6 +18,8 @@ func _ready():
 
 
 func _on_start_btn_button_up():
+	ClickSound.stream = load("res://assets/sound/reload2.wav")
+	ClickSound.play()
 	Global.player_type = 1
 	get_tree().change_scene_to_file("res://Scenes/Select.tscn")
 	pass # Replace with function body.
