ff97d3d52f93f6d080166ab2ff4725bdaaef9522
Knyak <ufshg@naver.com>
2023-12-07 01:11:34 +0900

    랭킹페이지 초기값 주입

--- Scripts/GameScripts/Ranking.gd
+++ Scripts/GameScripts/Ranking.gd
@@ -9,3 +9,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_http_request_request_completed(result, response_code, headers, body):
+	pass # Replace with function body.
